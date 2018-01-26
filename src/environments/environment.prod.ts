3db5e4230ac45aef853312baef370945e1924a17
PauMB <paumb85@gmail.com>
2018-01-26 11:57:24 +0100

    add dependencies firebase and angularfirebase2, add the config variables into the envirioment file and finally, add the module ins app.module file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBN8SIxBPivZHRl9gO-rVaZ96T6Rvr4gwU',
+    authDomain: 'oshop-1544b.firebaseapp.com',
+    databaseURL: 'https://oshop-1544b.firebaseio.com',
+    projectId: 'oshop-1544b',
+    storageBucket: '',
+    messagingSenderId: '978554361847'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBN8SIxBPivZHRl9gO-rVaZ96T6Rvr4gwU',
+    authDomain: 'oshop-1544b.firebaseapp.com',
+    databaseURL: 'https://oshop-1544b.firebaseio.com',
+    projectId: 'oshop-1544b',
+    storageBucket: '',
+    messagingSenderId: '978554361847'
+  }
 };
